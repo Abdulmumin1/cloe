580be60a558a1ee8828f335c8eec03f83d435d01
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-06-16 18:44:00 +0100

    ui changes

--- src/components/every.svelte
+++ src/components/every.svelte
@@ -3,7 +3,7 @@
   let selectedOption = "Explain this";
 
   const configuration = new Configuration({
-    apiKey: "YOUR_API_KEY",
+    apiKey: "YOUR_KEY",
   });
   const openai = new OpenAIApi(configuration);
 
@@ -34,52 +34,56 @@
   }
 </script>
 
-<div class="bg-gray-900 p-10 w-[20rem] flex flex-col text-orange-50">
+<div class=" p-4 bg-[#333] text-orange-50 w-[20rem] flex flex-col rounded-xl">
   <div class="inline-block">
-    <h1>Cloe</h1>
+    <!-- <h1>Cloe</h1> -->
     <i>Clever AI patner</i>
   </div>
-  <div class="flex items-center space-x-4 mb-3">
-    <label
-      class="inline-flex cursor-pointer items-center bg-gray-800 border border-black rounded-full p-2"
-    >
-      <input
-        type="radio"
-        class="form-radio text-blue-500"
-        name="option"
-        bind:group={selectedOption}
-        value="Explain this"
-        on:change={handleOptionChange}
-      />
-      <span class="ml-2">Explain</span>
-    </label>
-    <label
-      class="inline-flex cursor-pointer items-center bg-gray-800 border border-black rounded-full p-2"
-    >
-      <input
-        type="radio"
-        class="form-radio text-blue-500"
-        name="option"
-        bind:group={selectedOption}
-        value="Summarize this"
-        on:change={handleOptionChange}
-      />
-      <span class="ml-2">Summarize</span>
-    </label>
-  </div>
+
   <textarea
     bind:value={inputText}
     on:input={handleInput}
-    class="bg-inherit border-2 rounded-lg border-gray-600 h-12"
+    class="bg-inherit border-2 rounded-lg mb-2 border-gray-600 h-20"
   />
-  <button
-    id="getSelectedText"
-    on:click={print_me}
-    class="text-xl bg-gray-600 text-gray-900 mt-2 rounded-lg hover:bg-gray-500 transition-transform transform hover:scale-105"
-  >
-    Tell me
-  </button>
-  <p id="result text-wrap w-full">{result}</p>
+
+  <div class="flex items-center space-x-2">
+    <div class="flex items-center space-x-4 h-full">
+      <label
+        class="inline-flex cursor-pointer items-center bg-orange-200 border-black rounded-full p-2"
+      >
+        <input
+          type="radio"
+          class="form-radio"
+          name="option"
+          bind:group={selectedOption}
+          value="Explain this"
+          on:change={handleOptionChange}
+        />
+        <span class="ml-2 text-black">Explain</span>
+      </label>
+      <label
+        class="inline-flex cursor-pointer items-center bg-rose-600 border-black rounded-full p-2"
+      >
+        <input
+          type="radio"
+          class="form-radio"
+          name="option"
+          bind:group={selectedOption}
+          value="Summarize this"
+          on:change={handleOptionChange}
+        />
+        <span class="ml-2">Summarize</span>
+      </label>
+    </div>
+    <button
+      id="getSelectedText"
+      on:click={print_me}
+      class="text-xl w-full h-full bg-green-300 text-gray-900 rounded-lg hover:bg-gray-500 transition-transform transform hover:scale-105"
+    >
+      Go
+    </button>
+  </div>
+  <p id="result" class="rounded-lg bg-[#444] mt-2 p-2">{result}</p>
 </div>
 
 <style>
